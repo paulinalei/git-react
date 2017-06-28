4307ebc08df464911c553453298ebcb68f5d518e
Paulina Lei <paulina.y.lei@gmail.com>
2017-06-28 06:51:37 +0100

    setup react, react-dom, babel, and webpack

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+module.exports = App;
