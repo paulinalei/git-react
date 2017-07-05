003db90255417ca06ee51f911272082d3464365a
Paulina Lei <paulina.y.lei@gmail.com>
2017-07-05 04:22:15 +0100

    getting popular repos

--- utils/api.js
+++ utils/api.js
@@ -3,3 +3,14 @@ var axios = require("axios");
 var clientID = CLIENT_ID;
 var clientSecret = CLIENT_SECRET;
 var params = "?client_id=" + id + "&client_secret=" + sec;
+
+module.exports = {
+  getPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.items;
+      });
+  }
+};
